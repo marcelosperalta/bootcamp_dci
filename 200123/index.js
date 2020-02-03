ac84152f4e638e1faa42fd254e74070d56bc3950
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2020-02-03 22:54:37 +0100

    Update index.js

--- 200123/index.js
+++ 200123/index.js
@@ -148,17 +148,17 @@ let newRevStr = str3
     .join("");
 console.log(newRevStr); // tpircSavaJ
 
-/*
-c4n y0u r34d th15?
-Create a function that takes a string as an argument
-and returns a coded (h4ck3r 5p34k) version of the string.
-NB: for your program to work properly, the function should replace all
-’a’s with 4, ’e’s with 3, ’i’s with 1, ’o’s with 0, and ’s’s with 5.
-Examples:
-hackerSpeak(“javascript is cool”) ➞ “j4v45cr1pt 15 c00l”
-hackerSpeak(“programming is fun”) ➞ “pr0gr4mm1ng 15 fun”
-hackerSpeak(“become a coder”) ➞ “b3c0m3 4 c0d3r”
-*/
+// --------------------------------------------------------------------------------
+// 23.01.2020 - 01
+// c4n y0u r34d th15?
+// Create a function that takes a string as an argument
+// and returns a coded (h4ck3r 5p34k) version of the string.
+// NB: for your program to work properly, the function should replace all
+// ’a’s with 4, ’e’s with 3, ’i’s with 1, ’o’s with 0, and ’s’s with 5.
+// Examples:
+// hackerSpeak(“javascript is cool”) ➞ “j4v45cr1pt 15 c00l”
+// hackerSpeak(“programming is fun”) ➞ “pr0gr4mm1ng 15 fun”
+// hackerSpeak(“become a coder”) ➞ “b3c0m3 4 c0d3r”
 
 const hacking = (str) => {
     let hackedArr = [];
@@ -187,19 +187,19 @@ const hacking = (str) => {
 let codeStr = "Hi, I am nice aaa";
 console.log(hacking(codeStr));     // h1, 1 4m n1c3 444
 
-/*
-Is it Symmetrical?
-Create a function that takes a number as an argument
-and returns true or false depending on whether the number is symmetrical or not.
- NB(Nota Bene: used to mark something as particularly important):
- A number is symmetrical when it is the same as its reverse.
-Examples:
-isSymmetrical(7227) ➞ true
-isSymmetrical(12567) ➞ false
-isSymmetrical(44444444) ➞ true
-isSymmetrical(9939) ➞ false
-isSymmetrical(1112111) ➞ true
-*/
+// --------------------------------------------------------------------------------
+// 23.01.2020 - 02
+// Is it Symmetrical?
+// Create a function that takes a number as an argument
+// and returns true or false depending on whether the number is symmetrical or not.
+//  NB(Nota Bene: used to mark something as particularly important):
+//  A number is symmetrical when it is the same as its reverse.
+// Examples:
+// isSymmetrical(7227) ➞ true
+// isSymmetrical(12567) ➞ false
+// isSymmetrical(44444444) ➞ true
+// isSymmetrical(9939) ➞ false
+// isSymmetrical(1112111) ➞ true
 
 function isSymmetrical(num){
     let numToStr = num.toString();
@@ -215,14 +215,14 @@ function isSymmetrical(num){
 console.log(isSymmetrical(9939)); // false
 console.log(isSymmetrical(9339)); // true
 
-/*
-snake_case ➞ camelCase
-Create a function toCamelCase() that takes a single string in snake_case
-and converts it into camelCase.
-Examples:
-toCamelCase(“hello_world”) ➞ “helloWorld”
-toCamelCase(“javascript_is_fun”) ➞ “javaScriptIsFun”
-*/
+// --------------------------------------------------------------------------------
+// 23.01.2020 - 03
+// snake_case ➞ camelCase
+// Create a function toCamelCase() that takes a single string in snake_case
+// and converts it into camelCase.
+// Examples:
+// toCamelCase(“hello_world”) ➞ “helloWorld”
+// toCamelCase(“javascript_is_fun”) ➞ “javaScriptIsFun”
 
 const toCamelCase = str => { // function ES6 style > Arrow functions
     let splitStr = str.split("_");
@@ -244,21 +244,21 @@ const toCamelCase = str => { // function ES6 style > Arrow functions
 }
 console.log(toCamelCase("snack_long_hi"));
 
-/*
-Pig Latin Translation.
-Create a function that takes a string of words
-and moves the first letter of each word to the end of it,
-then adds ‘ay’ to the end of the word.
-This is a basic form of “Pig Latin”.
-Move the first letter of each word to the end of the word.
-Add “ay” to the end of the word.
-Words starting with a vowel (A, E, I, O, U) append “way” to the end instead.
-Preserve proper capitalization as in the examples below.
-Examples:
-pigLatin(“Cats are great pets.“) ➞ “Atscay areway reatgay etspay.”
-pigLatin(“Tom got a small piece of pie.“) ➞ “Omtay otgay away allsmay iecepay ofway iepay.”
-pigLatin(“He told us a very exciting tale.“) ➞ “Ehay oldtay usway away eryvay excitingway aletay.”
-*/
+// --------------------------------------------------------------------------------
+// 23.01.2020 - 04
+// Pig Latin Translation.
+// Create a function that takes a string of words
+// and moves the first letter of each word to the end of it,
+// then adds ‘ay’ to the end of the word.
+// This is a basic form of “Pig Latin”.
+// Move the first letter of each word to the end of the word.
+// Add “ay” to the end of the word.
+// Words starting with a vowel (A, E, I, O, U) append “way” to the end instead.
+// Preserve proper capitalization as in the examples below.
+// Examples:
+// pigLatin(“Cats are great pets.“) ➞ “Atscay areway reatgay etspay.”
+// pigLatin(“Tom got a small piece of pie.“) ➞ “Omtay otgay away allsmay iecepay ofway iepay.”
+// pigLatin(“He told us a very exciting tale.“) ➞ “Ehay oldtay usway away eryvay excitingway aletay.”
 
 function pigLatin(str) {
     let translation = [];
