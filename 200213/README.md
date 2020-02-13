08acc7d066bba460c5fa2539e3c3191eb667f343
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2020-02-13 10:11:11 +0100

    Add files via upload

---
+++ 200213/README.md
@@ -0,0 +1,21 @@
+# JavaScript
+
+### try...catch
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+
+### throw
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+
+### setTimeout()
+
+https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout
+
+### freeCodeCamp
+
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/
+
+### Code
+
+see index.js (https://github.com/marcelosperalta/dci/blob/master/200213/index.js)
---
+++ 200213/index.js
@@ -0,0 +1,55 @@
+
+// errors
+
+// try...catch     // throw
+
+try {
+  console.log("Hi his is trying phase");
+  cool()
+  console.log("2nd phase");                 // if there is an error before this line, this part will never run
+} catch (err) {
+  console.log("This error is" + err)
+}
+
+console.log(``);
+
+let userName = "";
+try {
+  console.log("Hi his is trying phase");
+  if (userName == "") throw " Sorry something went wrong ";
+  console.log("2nd phase");
+} catch (err) {
+  console.log("This error is" + err)
+} finally {
+  console.log(`This will always run`);
+}
+
+console.log(``);
+
+
+// call a function several times
+
+function display () {
+  console.log("Hi this is a cool function")
+}
+for (let i = 0; i < 5; i++) {display()}
+
+console.log(``);
+
+(function manyTimesCalledFunction(num){
+  display();
+  if (num > 1) manyTimesCalledFunction (num -1);
+})(5);
+
+console.log(``);
+
+
+// setTimeout()
+
+function print() {
+  console.log("Hi I am JS");
+}
+const timerPrint = setTimeout(print, 500);
+
+const timerDisplay = setTimeout(() => console.log("Cool thats you have waited me"), 1000);
+//clearTimeout(timerDisplay);
