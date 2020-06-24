29326af72851d9eb2c6bc15989d15e5df6cd5f54
marcelosperalta <marcelosperalta@gmail.com>
2020-06-24 22:09:24 +0200

    React

--- 200624/src/components/ProductItem.js
+++ 200624/src/components/ProductItem.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const ProductItem = (props) => {
     const { id, productName, icon, price, inventory } = props.info;
-
     return (
         <div className="container" key={id}>
             <table>
                 <tr>
                     <td className="iconProduct">{icon}</td>
                     <td className="productName">{productName}</td>
-                    <td>{price}</td>
+                    <td className="price">{price}</td>
                     <td>
                         <button disabled={inventory === 0}>
                             {inventory > 0 ? "Add to cart" : "Sold out"}
                         </button>
                     </td>
+                    <td className="inventory">{inventory}</td>
                 </tr>
             </table>
         </div>
