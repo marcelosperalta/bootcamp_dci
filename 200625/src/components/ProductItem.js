63df740343a2ca7e4fb4993c107642e2830d8946
marcelosperalta <marcelosperalta@gmail.com>
2020-06-29 10:55:34 +0200

    React

--- 200625/src/components/ProductItem.js
+++ 200625/src/components/ProductItem.js
@@ -23,3 +23,21 @@ const ProductItem = (props) => {
 };
 
 export default ProductItem;
+
+// import React from "react";
+
+// const ProductItem = (props) => {
+//   const { id, productName, icon, price, inventory } = props.info;
+
+//   return (
+//     <li key={id}>
+//       {productName} <i>{icon}</i>
+//       <h5>{price}</h5>
+//       <button disabled={inventory === 0}>
+//         {inventory > 0 ? "Add to cart" : "Sold out"}
+//       </button>
+//     </li>
+//   );
+// };
+
+// export default ProductItem;
--- 200625/src/components/ProductList.js
+++ 200625/src/components/ProductList.js
@@ -13,3 +13,31 @@ export default class ProductList extends React.Component {
         return <div>{items}</div>;
     }
 }
+
+// import React from "react";
+// import ProductItem from "./ProductItem";
+
+// export default class ProductList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       cart: [],
+//       totalPrise: 0,
+//     };
+//   }
+//   addToCart = (item) => {
+//     // const arr = [...this.state.cart, item];
+//     this.setState({
+//       cart: [...this.state.cart, item],
+//     });
+//     console.log(this.state.cart);
+//   };
+
+//   render() {
+//     const items = this.props.data.map((item, i) => {
+//       return <ProductItem key={i} info={item} />;
+//     });
+
+//     return <ul>{items}</ul>;
+//   }
+// }
